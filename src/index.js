f327dfc5e410b375f01ba27ed8059db6923c6d23
rmahawewa <rmahawewa@yahoo.com>
2024-09-22 23:22:19 +0530

    create ui with js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import "./styles.css";
+import { create_menu } from "./menu.js";
+
+let option = document.querySelectorAll(".nav-btn");
+// console.log(option);
+
+let add_content = (id) => {
+    let content = document.querySelector("#content");
+    let temp = document.createElement("template");
+    content.innerHTML=temp;
+    if(id.localeCompare("home-btn") === 0){
+
+    }
+    if(id.localeCompare("menu-btn") === 0){
+        temp.innerHTML = create_menu;
+    }
+    if(id.localeCompare("about-btn") === 0){
+        // content.innerHTML = ;
+    }
+};
+
+// option.forEach((item) => item.addEventListner("click", add_content(item.id)));
+option.forEach((item) => item.addEventListener("click", function(){
+    add_content(item.id);
+}));
+
+// document.querySelector("#home-btn").addEventListener("click", function(){});
+// document.querySelector("#menu-btn").addEventListener("click", function(){add_content("menu-btn")});
+// document.querySelector("#about-btn").addEventListener("click", function(){});
 
 console.log("Hello Friends!");
---
+++ src/menu.js
@@ -0,0 +1,11 @@
+let create_menu = `
+        <div class="menu" >
+            <div class="breakfast" ></div>
+            <div class="lunch" ></div>
+            <div class="dinner" ></div>
+            <div class="beverages" ></div>
+            <div class="desserts" ></div>
+        </div>
+    `;
+
+ export {create_menu};
