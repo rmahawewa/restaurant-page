b87e5ab7772f4cc6462d4ea52809e432971f4408
rmahawewa <rmahawewa@yahoo.com>
2024-09-24 05:51:41 +0530

    add ui functionalities

--- src/content.js
+++ src/content.js
@@ -1,27 +1,35 @@
-function itm(itmid) {
+import {list_breakfast_items} from "./food_items.js";
+
+function itm(itmid,img_src,name,description,price) {
     let item = `<div class='item'>
-                                <div id=`+ itmid +` class="img-div"></div>
-                                <label class='itm-name'>item</label>
+                                <img id=`+ itmid +` class="img" src=`+ img_src +`>
+                                <label class='itm-name'>`+ name +`</label>
                                 <button class='`+ itmid +`-dtls'>Details</button>
-                                <label class='price ` + itmid + `'> per one (50% off for first three days)</label>
+                                <label class='price ` + itmid + `'>`+ price +` per one (50% off for first three days)</label>
                 </div>`;
     return {item};
 }
 
-function food_items(time) {    
+function food_items(time, menu_arr) {    
     let list = () => {
         let brkfst = '';
-        for(let i=0; i<12; i++){
+        for(let i=0; i<menu_arr.length; i++){
             let num = i + 1;
             let itmid = time + num;
-            brkfst += itm(itmid).item;
+            let img_src = menu_arr[i].image_src;
+            let item_name = menu_arr[i].name;
+            let description = menu_arr[i].description;
+            let price = menu_arr[i].price;
+            brkfst += itm(itmid, img_src, item_name, description, price).item;
         }
         return brkfst;
     }    
     return {list};
 }
 
-let brft = food_items("bft");
+let breakfast_items = list_breakfast_items;
+let brft = food_items("bft", breakfast_items);
+
 
 let create_menu = `<div class="meal-time">
                         <label id="bfst">Breakfast</label>
